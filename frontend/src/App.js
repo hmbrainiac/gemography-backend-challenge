9cff2db7a09d8ad8087640cc9eaaab9ea3386750
Abidar El Mehdi <abidar.mehdi95@gmail.com>
2020-03-31 23:34:25 +0100

    initiate frontend part

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-xl text-indigo-500 mt-12">My attempt here</h1>
+    </div>
+  );
+}
+
+export default App;
