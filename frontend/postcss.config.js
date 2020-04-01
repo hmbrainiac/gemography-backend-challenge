3b6bafa954ef8297b58f1a5f0d2ef256bfa37b9e
Abidar El Mehdi <abidar.mehdi95@gmail.com>
2020-04-01 23:17:08 +0100

    slim css file with purgecss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,23 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project 
+    content: [
+        './src/**/*.js',
+        './src/**/*.jsx',
+        './public/index.html',
+    ],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-:/]+(?<!:)/g) || []
+})
 const tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.config.js'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
     ],
 };
