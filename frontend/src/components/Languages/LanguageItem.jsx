1fb142e4b05ba39bf27f918fcbecaf30fe01263f
Abidar El Mehdi <abidar.mehdi95@gmail.com>
2020-04-01 17:21:29 +0100

    fetch languages with axios

--- frontend/src/components/Languages/LanguageItem.jsx
+++ frontend/src/components/Languages/LanguageItem.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Repos from "./Repos"
-function LanguageItem({ language, repos }) {
+
+function LanguageItem({ language, nbr_user, repos }) {
   return (
-    <li className="border-t border-gray-200">
+    <li key={language} className="border-t border-gray-200">
       <div className="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
         <div className="px-4 py-4 sm:px-6">
           <div className="flex items-center justify-between">
@@ -10,8 +11,8 @@ function LanguageItem({ language, repos }) {
               {language}
             </div>
             <div className="ml-2 flex-shrink-0 flex">
-              <span class="inline-flex items-center px-4 py-1 rounded-full text-sm font-medium leading-5 bg-indigo-100 text-indigo-800">
-                Used by {repos.length}
+              <span className="inline-flex items-center px-4 py-1 rounded-full text-sm font-medium leading-5 bg-indigo-100 text-indigo-800">
+                Used by {nbr_user}
               </span>
             </div>
           </div>
--- frontend/src/components/Languages/Languages.jsx
+++ frontend/src/components/Languages/Languages.jsx
@@ -1,35 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+
 import LanguageItem from "./LanguageItem";
 
 
 function Languages() {
-  const languages = [
-    {
-      language: "C++",
-      nbr_used: 2,
-      repos: [
-        'git://github.com/MegEngine/MegEngine.git',
-        'git://github.com/Jittor/jittor.git'
-      ]
-    },
-    {
-      language: "Jupyter Notebook",
-      nbr_used: 4,
-      repos: [
-        'git://github.com/google/automl.git',
-        'git://github.com/jcl5m1/ventilator.git',
-        'git://github.com/bmild/nerf.git',
-        'git://github.com/github/covid19-dashboard.git'
-      ]
-    },
-    {
-      language: "Dart",
-      nbr_used: 1,
-      repos: [
-        'git://github.com/WorldHealthOrganization/app.git'
-      ]
-    }
-  ]
+  const languagesEndpoint = "http://localhost:8000"
+  const [languages, setLanguages] = useState([]);
+
+  useEffect(() => {
+    axios.get(languagesEndpoint)
+      .then(response => {
+        setLanguages(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  });
+
   return (
     <div className="w-full mx-auto mt-20 bg-white shadow overflow-hidden sm:rounded-md">
       <div className="px-4 py-5">
@@ -40,7 +28,7 @@ function Languages() {
         </div>
       </div>
       <ul>
-        {languages.map((item) => <LanguageItem language={item.language} repos={item.repos} />)}
+        {languages.map((item) => <LanguageItem language={item.language} nbr_used={item.nbr_used} repos={item.repos} />)}
       </ul>
     </div>
   );
