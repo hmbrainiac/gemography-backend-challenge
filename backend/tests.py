5546c2d765585e44987e645a5ea901db567c5db1
WORK@DELL <abidar.mehdi95@gmail.com>
2020-04-03 01:08:16 +0100

    update endpoints URI in tests

--- backend/tests.py
+++ backend/tests.py
@@ -5,11 +5,16 @@ from main import app
 client = TestClient(app)
 
 
-def test_laguages():
+def test_index():
     response = client.get("/")
     assert response.status_code == 200
 
 
+def test_laguages():
+    response = client.get("api/get_languages")
+    assert response.status_code == 200
+
+
 def test_laguages_with_nbr_days():
-    response = client.get("/40")
+    response = client.get("api/get_languages/40")
     assert response.status_code == 200
